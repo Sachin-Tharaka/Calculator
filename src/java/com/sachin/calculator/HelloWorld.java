5a3cc816a1026f43cf6c29040dd91c348a018320
Sachin-Tharaka <tharakasachin98@gmail.com>
2021-08-08 13:01:24 +0530

    created calculator

--- src/java/com/sachin/calculator/HelloWorld.java
+++ src/java/com/sachin/calculator/HelloWorld.java
@@ -3,7 +3,9 @@ package com.sachin.calculator;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println("Enter the first number:");
+
 
     }
 }
